6f245441cb0afa4af07b893ae2e52712252c4047
Teja <muskuraviteja999@gmail.com>
2026-04-25 12:17:44 +0530

    Changing Images

---
+++ addingImages/addingImages/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  addingImages
+//
+//  Created by Raviteja Reddy on 25/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var message = ""
+    @State private var imageString = ""
+    
+    var body: some View {
+        
+        Spacer()
+        
+        VStack{
+            Image(imageString)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+            
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Spacer()
+            
+            Button("Change Image") {
+                let message1 = "You are awesome"
+                let message2 = "You are great"
+                let imageString1 = "image0"
+                let imageString2 = "image1"
+                
+                message = (message == message1 ? message2 : message1)
+                imageString = (imageString == imageString1 ? imageString2 : imageString1)
+            }
+            
+            
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ addingImages/addingImages/addingImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  addingImagesApp.swift
+//  addingImages
+//
+//  Created by Raviteja Reddy on 25/04/26.
+//
+
+import SwiftUI
+
+@main
+struct addingImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
