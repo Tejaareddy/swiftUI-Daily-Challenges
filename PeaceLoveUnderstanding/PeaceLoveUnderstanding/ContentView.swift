b3cace578ce2532c4d7763efeec1940d7a16e64d
Teja <muskuraviteja999@gmail.com>
2026-04-25 08:58:18 +0530

    PeaceLove

---
+++ PeaceLoveUnderstanding/PeaceLoveUnderstanding/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  PeaceLoveUnderstanding
+//
+//  Created by Raviteja Reddy on 24/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var textMessage = "Peacefull"
+    @State private var image = "peacesign"
+    var body: some View {
+        VStack{
+            Text("Hey this is a small game")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.blue)
+            
+            Spacer()
+
+            
+            Image(systemName: image)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 350, height: 350)
+       
+            
+            Text(textMessage)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.orange)
+            Spacer()
+        }
+        
+        
+        HStack {
+            
+            Button("Love"){
+                textMessage = "I love you"
+                image = "heart"
+            }
+            
+            Button("Hate"){
+                textMessage = "I hate you"
+                image = "heart.fill"
+            }
+            
+            Button("Peace"){
+                textMessage = "I Want Peace"
+                image = "peacesign"
+            }
+
+
+            
+        }
+        .buttonStyle(.borderedProminent)
+        
+        
+        
+        
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PeaceLoveUnderstanding/PeaceLoveUnderstanding/PeaceLoveUnderstandingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeaceLoveUnderstandingApp.swift
+//  PeaceLoveUnderstanding
+//
+//  Created by Raviteja Reddy on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PeaceLoveUnderstandingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
